2095ba09b22aba27032e0443bafa89672ccb00dc
Naomi <naomi@noncepad.com>
2024-02-01 19:09:36 +0900

    convert func converts mkv to gif

--- converter/converter.go
+++ converter/converter.go
@@ -1,7 +1,56 @@
 package converter
 
-type Converter struct {
+import (
+	"context"
+	"fmt"
+	"os"
+	"os/exec"
+)
+
+type Converter interface {
+	// takes an mp4 inputfile and converts it to a gif output file
+	Convert(ctx context.Context, inputFile, outputFile string) error
 }
 
-type CreateConverter struct {
+type simpleConverter struct {
+	ctx    context.Context
+	config *Configuration
+}
+
+type Configuration struct {
+}
+
+func CreateSimpleConverter(ctx context.Context, config *Configuration) (Converter, error) {
+	e1 := new(simpleConverter)
+	e1.ctx = ctx
+	e1.config = config
+	return e1, nil
+}
+
+// we need to put a context variable here
+// the sc.ctx is for the server as a whole.
+// ctx is for the user request.
+func (sc *simpleConverter) Convert(ctx context.Context, inputFile, outputFile string) error {
+	// use create to make sure we are not overwriting a file
+	// the command will cail if the output file already exists
+	saveFileHandle, err := os.Create(outputFile)
+	if err != nil {
+		return err
+	}
+
+	// OLD: ffmpeg -i ./files/solpipe.mkv ./files/solpipe2.gif
+	// new: ffmpeg -i ./files/solpipe.mp4 -f gif pipe:1 > ./files/solpipe5.gif
+	cmd := exec.CommandContext(ctx, "ffmpeg", "-i", inputFile, "-f", "gif", "pipe:1")
+
+	// redirect stdout to the save handle
+	cmd.Stdout = saveFileHandle
+	cmd.Stderr = os.Stderr
+	err = cmd.Start()
+	if err != nil {
+		err = fmt.Errorf("ffmpeg command failed: %v", err)
+		return err
+	}
+
+	fmt.Println("Conversion successful")
+	return cmd.Wait()
 }
--- converter/converter_test.go
+++ converter/converter_test.go
@@ -1,15 +1,18 @@
 package converter_test
 
 import (
+	"context"
 	"os"
 	"os/exec"
 	"testing"
+
+	pkgcvr "gitlab.noncepad.com/naomiyoko/ffmpeg-market/converter"
 )
 
 func TestFFmpegConversion(t *testing.T) {
 	// Define input and output file paths
-	inputFile := "/home/naomi/work/ffmpeg-market/local-files/solpipe.mkv"
-	outputFile := "/home/naomi/work/ffmpeg-market/local-files/solpipe.gif"
+	inputFile := "../files/solpipe.mp4"
+	outputFile := "../files/solpipe2.gif"
 
 	// Construct FFmpeg command
 	cmd := exec.Command("ffmpeg", "-i", inputFile, outputFile)
@@ -19,12 +22,41 @@ func TestFFmpegConversion(t *testing.T) {
 
 	// Check if FFmpeg command produced any errors
 	if err != nil {
-		t.Errorf("FFmpeg command failed: %v", err)
+		t.Fatalf("FFmpeg command failed: %v", err)
 	}
 
 	// Check if the output file exists after conversion
 	_, err = os.Stat(outputFile)
 	if err != nil {
+		t.Fatalf("Output file does not exist: %v", err)
+	}
+}
+
+func TestConvert(t *testing.T) {
+
+	// Define input and output file paths
+	// inputfile must exist, outputfile must not exist
+	inputFile := "../files/solpipe.mp4"
+	outputFile := "../files/solpipe2.gif"
+
+	// Create a simple converter instance
+	ctx := context.Background()
+	// make config a reference
+	config := &pkgcvr.Configuration{}
+	// create a converter with CreateConverter function call
+	converter, err := pkgcvr.CreateSimpleConverter(ctx, config)
+	if err != nil {
+		t.Fatalf("Failed to create converter: %v", err)
+	}
+
+	// Call the Convert function
+	err = converter.Convert(ctx, inputFile, outputFile)
+	if err != nil {
+		t.Errorf("Convert function returned an error: %v", err)
+	}
+
+	// Check if the output file exists
+	if _, err := os.Stat(outputFile); os.IsNotExist(err) {
 		t.Errorf("Output file does not exist: %v", err)
 	}
 }
