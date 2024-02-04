2fd76c80e925f35b92eaac7caff7a12ae544edfa
Naomi <naomi@noncepad.com>
2024-02-04 22:14:51 +0900

    run function

--- converter/converter.go
+++ converter/converter.go
@@ -3,13 +3,14 @@ package converter
 import (
 	"context"
 	"fmt"
+	"io"
 	"os"
 	"os/exec"
 )
 
 type Converter interface {
 	// takes an mp4 inputfile and converts it to a gif output file
-	Convert(ctx context.Context, inputFile, outputFile string) error
+	Convert(ctx context.Context, inputFile string, ext string) (io.ReadCloser, error)
 	Render(ctx context.Context, inputFile, outputFile string) error
 }
 
@@ -31,29 +32,26 @@ func CreateSimpleConverter(ctx context.Context, config *Configuration) (Converte
 // we need to put a context variable here
 // the sc.ctx is for the server as a whole.
 // ctx is for the user request.
-func (sc *simpleConverter) Convert(ctx context.Context, inputFile, outputFile string) error {
+func (sc *simpleConverter) Convert(ctx context.Context, inputFile string, ext string) (io.ReadCloser, error) {
 	// use create to make sure we are not overwriting a file
 	// the command will fail if the output file already exists
-	saveFileHandle, err := os.Create(outputFile)
-	if err != nil {
-		return err
-	}
+	saveHandle, stdout := io.Pipe()
 
 	// OLD: ffmpeg -i ./files/solpipe.mkv ./files/solpipe2.gif
 	// new: ffmpeg -i ./files/solpipe.mp4 -f gif pipe:1 > ./files/solpipe5.gif
-	cmd := exec.CommandContext(ctx, "ffmpeg", "-i", inputFile, "-f", "gif", "pipe:1")
+	cmd := exec.CommandContext(ctx, "ffmpeg", "-i", inputFile, "-f", ext, "pipe:1")
 
 	// redirect stdout to the save handle
-	cmd.Stdout = saveFileHandle
+	cmd.Stdout = stdout
 	cmd.Stderr = os.Stderr
-	err = cmd.Start()
+	err := cmd.Start()
 	if err != nil {
 		err = fmt.Errorf("ffmpeg command failed: %v", err)
-		return err
+		return nil, err
 	}
 
 	fmt.Println("Conversion successful")
-	return cmd.Wait()
+	return saveHandle, cmd.Wait()
 }
 func (sc *simpleConverter) Render(ctx context.Context, inputFile, outputFile string) error {
 	// use create to make sure we are not overwriting a file
@@ -62,7 +60,6 @@ func (sc *simpleConverter) Render(ctx context.Context, inputFile, outputFile str
 	if err != nil {
 		return err
 	}
-	// This command is not working, i am trying to find the right one
 	// blender -b ./files/solpop.blend -o ./files/solpop.avi -F AVIJPEG -x 1 -f 1 -a
 	cmd := exec.CommandContext(ctx, "blender", "--b", inputFile, "-out", outputFile, "-F", "AVIJPEG", "-x", "1", "-f", "1", "-a")
 
--- worker/worker.go
+++ worker/worker.go
@@ -3,6 +3,8 @@ package worker
 import (
 	"context"
 	"io"
+
+	"gitlab.noncepad.com/naomiyoko/ffmpeg-market/converter"
 )
 
 type Job struct {
@@ -22,4 +24,62 @@ type Worker interface {
 	Run(Job) Result
 	// this is ther workers version of ctx.Done()
 	CloseSignal() <-chan error
+}
+
+type simpleWorker struct {
+	ctx context.Context
+	c   converter.Converter
+	tmp string
+}
+
+func Create(ctx context.Context, c converter.Converter, tmp string) (Worker, error) {
+	e1 := new(simpleWorker)
+	e1.ctx = ctx
+	e1.c = c
+	e1.tmp = tmp
+	return e1, nil
+}
+
+func (sw *simpleWorker) Run(job Job) Result {
+	// call render to convert blender file to avi
+	err := sw.c.Render(job.Ctx, job.Blender, sw.tmp)
+	if err != nil {
+		return Result{Reader: nil, Err: err}
+	}
+	// Define an array of file handles (Reader)
+	readerList := make([]io.ReadCloser, len(job.Out))
+
+	// call convert to convert avi (in working dir) to mp4, mkv, gif
+	for i, fileExtension := range job.Out {
+		fileHandle, err2 := sw.c.Convert(job.Ctx, sw.tmp+"/solpop.avi", fileExtension)
+		if err2 != nil {
+			// if an error occurs, close all handles and retun err
+			for _, reader := range readerList {
+				if reader != nil {
+					reader.Close()
+				}
+			}
+			return Result{Reader: nil, Err: err}
+		}
+		readerList[i] = fileHandle
+	}
+	return Result{Reader: readerList, Err: nil}
+
+}
+
+func (sw *simpleWorker) CloseSignal() <-chan error {
+	// Create a channel to receive signals
+	signalC := make(chan error)
+
+	// Run a goroutine to monitor for context cancellation or errors
+	go func() {
+		// Block until the worker's context is cancelled or an error occurs
+		<-sw.ctx.Done()
+
+		// Send a signal to the channel if to close
+		signalC <- sw.ctx.Err()
+	}()
+
+	// Return the channel for receiving signals
+	return signalC
 }
